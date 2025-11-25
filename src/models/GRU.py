5d3ded6eab9616e3ae6ca878b222500c6ba0eb33
Shivraj_Mate <shivay1107@gmail.com>
2025-11-25 13:14:23 +0530

    EDA Pipeline Start

--- src/models/GRU.py
+++ src/models/GRU.py
@@ -1,65 +1,51 @@
-import keras_tuner as kt
 import tensorflow as tf
-from tensorflow import keras
 from keras.models import Sequential
-from keras.layers import Embedding, GRU, Dense, Dropout
+from keras.layers import Embedding, GRU, Dense
 from keras.optimizers import Adam
 
 class GRUModel:
-    def __init__(self, vocab_size=500000,  max_len=100, embedding_dim = 256, Dropout_l = 0.1, units=256, step=0.1,gru_dropout=0.1,recurrent_dropout=0.1,
-                 dense_units=128, dense_dropout=0.1, activation='relu'):
+    def __init__(self, vocab_size=500000, max_len=100, embedding_dim=[64,128,256],
+                 num_classes=2, recurrent_dropout=0.1, loss=None):
+        
         self.vocab_size = vocab_size
         self.max_len = max_len
         self.embedding_dim = embedding_dim
-        self.Dropout = Dropout_l
-        self.units = units
-        
-        
+        self.num_classes = num_classes
+        self.recurrent_dropout = recurrent_dropout
+        self.loss = loss
+
     def build(self, hp):
         model = Sequential()
+        
+        # Embedding layer
+        model.add(Embedding(
+            input_dim=self.vocab_size,
+            output_dim=hp.Choice('embedding_dim', self.embedding_dim),
+            input_length=self.max_len
+        ))
 
-        # Embedding Layer
-        model.add(
-            Embedding(
-                input_dim=self.vocab_size,
-                output_dim=self.embedding_dim,
-                input_length=self.max_len
-            )
-        )
-
-        model.add(Dropout(self.dropout_rate))
-
-        # GRU Layer
-        model.add(
-            GRU(
-                self.gru_units,
-                dropout=self.dropout_rate,
-                recurrent_dropout=self.dropout_rate,
-                return_sequences=self.return_sequences
-            )
-        )
+        # GRU layers
+        num_layers = hp.Int('num_gru_layers', 1, 3)
+        
+        
+        for i in range(num_layers):
+            return_seq = True if i < num_layers - 1 else False
+            model.add(GRU(
+                units=hp.Int(f'gru_units_{i+1}', min_value=32, max_value=128, step=32),
+                return_sequences=return_seq,
+                dropout=hp.Float(f'dropout_{i+1}', 0.1, 0.5, step=0.1),
+                recurrent_dropout=self.recurrent_dropout
+            ))
 
         # Output layer
         activation = 'softmax' if self.num_classes > 2 else 'sigmoid'
-        loss = 'categorical_crossentropy' if self.num_classes > 2 else 'binary_crossentropy'
-
+        loss = self.loss or ('categorical_crossentropy' if self.num_classes > 2 else 'binary_crossentropy')
         model.add(Dense(self.num_classes, activation=activation))
-
-        model.compile(loss=loss, optimizer=Adam(), metrics=['accuracy'])
-
-        return model
-    
-    
-    
-    def train(self, X_train, y_train, batch_size, epochs, validation_data):
-        return self.model.fit(
-            X_train,
-            y_train,
-            batch_size=batch_size,
-            epochs=epochs,
-            validation_data=validation_data,
-            verbose=1
+        
+        # Compile
+        model.compile(
+            optimizer=Adam(hp.Choice('learning_rate', [1e-2, 1e-3, 1e-4])),
+            loss=loss,
+            metrics=['accuracy']
         )
-
-    def evaluate(self, X_test, y_test):
-        return self.model.evaluate(X_test, y_test, verbose=1)
+        return model
